817487943bd00c04ad7468ef4cf33b824811e9b2
Blilliam <167144267+Blilliam@users.noreply.github.com>
2026-02-10 17:15:58 -0500

    Add files via upload

--- Virus/graph.py
+++ Virus/graph.py
@@ -6,33 +6,57 @@ class Graph:
         self.x = x
         self.y = y
 
+        self.x_scale = 1.0
+        self.x_scale_rate = 0  # how fast it squishes horizontally
+        self.hit_border = False
+
+
+
         self.w = w
         self.h = h
 
         self.game = game
 
     def draw(self, screen):
-        pygame.draw.rect(screen, [255, 255, 255], [self.x, self.y, self.w, self.h])
+        pygame.draw.rect(screen, [255, 255, 255], [self.x, self.y, self.w, self.h], 1)
 
-        self.barX = 1
+        bar_count = len(self.game.history)
 
-        for u, i, r in self.game.history:
-            self.total = u + i + r
+        # Calculate horizontal scale (only if needed)
+        if bar_count <= self.w:
+            x_scale = 1.0
+        else:
+            x_scale = self.w / bar_count  # squeeze JUST enough to fit
 
-            uPercent = u/self.total
-            iPercent = i/self.total
-            rPercent = r/self.total
+        bar_x = 0
+        
+        if self.game.history and self.game.history[-1][1] > 0:
+            for u, i, r in self.game.history:
+                total = u + i + r
 
-            uHeight = uPercent * self.h
-            iHeight = iPercent * self.h
-            rHeight = rPercent * self.h
+                if total == 0:
+                    continue
 
-            pygame.draw.rect(screen, [255, 255, 255], [self.barX + self.x, self.y, 1, uHeight])
-            pygame.draw.rect(screen, [255, 0, 0], [self.barX + self.x, self.y + uHeight, 1, iHeight])
-            pygame.draw.rect(screen, [0, 0, 0], [self.barX + self.x, self.y + uHeight + iHeight, 1, rHeight])
+                uHeight = (u / total) * self.h
+                iHeight = (i / total) * self.h
+                rHeight = (r / total) * self.h
+
+                draw_x = self.x + int(bar_x * x_scale)
+
+                if draw_x > self.x + self.w:
+                    break
+
+                pygame.draw.rect(screen, [255, 255, 255],
+                                [draw_x, self.y, 1, uHeight])
+                pygame.draw.rect(screen, [255, 0, 0],
+                                [draw_x, self.y + uHeight, 1, iHeight])
+                pygame.draw.rect(screen, [0, 0, 0],
+                                [draw_x, self.y + uHeight + iHeight, 1, rHeight])
+
+                bar_x += 1
 
 
-            self.barX += 1
+
 
         
 
